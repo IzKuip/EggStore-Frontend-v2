e92c22b844337e09ded138a43c789e01c0db5a4b
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-04-27 13:23:44 +0200

    Completely block v2

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,37 @@
 <script lang="ts">
-  import "./css/global.css";
+  import { onMount } from "svelte";
   import "./css/app.css";
-  import Content from "./lib/Content.svelte";
-  import Login from "./lib/Login.svelte";
-  import { isLoggedIn, sideBarOpened } from "./ts/env";
-import Confirmation from "./lib/Content/Confirmation.svelte";
+  import "./css/global.css";
+  import Confirmation from "./lib/Content/Confirmation.svelte";
+  import { showConfirmation } from "./ts/confirmation/main";
+  import { sideBarOpened } from "./ts/env";
+
+  onMount(() => {
+    showConfirmation({
+      title: "v2 Onbereikbaar",
+      message:
+        "De Reggistry is gemigreerd naar het CPB. Het is om die reden niet langer mogelijk om gebruik te maken van Reggistry v2. Gelieve Reggistry v3 of hoger te gebruiken.",
+      okButton: {
+        capt: "Herladen",
+        event() {
+          location.reload();
+        },
+      },
+      cancelButton: {
+        capt: "Reggistry v3",
+        event() {
+          location.href = "https://reggistry.twiserver.nl/";
+        },
+      },
+    });
+  });
 </script>
 
 <div class="app" class:nosidebar={!$sideBarOpened}>
-  {#if $isLoggedIn}
+  <!-- {#if $isLoggedIn}
     <Content />
   {:else}
     <Login />
-  {/if}
-  <Confirmation/>
+  {/if} -->
+  <Confirmation />
 </div>
